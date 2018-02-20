dc38ba49ebaccea108d4eac78db303297493c9a1
Maria <mariashklovskaya@coherentsolutions.com>
2018-02-20 18:00:18 +0300

    rolling-scope/feature: quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const eqationArr = equation.split(' ');
+  let a = parseInt(eqationArr[0]);
+  let b = parseInt(eqationArr[3] + eqationArr[4]);
+  let c = parseInt(eqationArr[7] + eqationArr[8]);
+  let Discr = b*b-4*a*c;
+  let result = [];
+  if(Discr > 0) {
+    Discr = Math.sqrt(Discr);
+    let x1 = (-b + Discr)/(2*a);
+    let x2 = (-b - Discr)/(2*a);
+    result.push(Math.round(x1), Math.round(x2));
+  }
+  return result.sort((a,b) => a-b);
 }
